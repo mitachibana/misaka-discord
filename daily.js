cd152eca55aad5d8a5dffdbe887adcb93d36ca9d
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2025-10-05 20:37:30 +0900

    Update daily.js

--- daily.js
+++ daily.js
@@ -2,7 +2,7 @@ const { tokenWolf, tokenExchange } = require("./config.json");
 
 async function daily() {
   const wolfTime1 = await fetch(
-    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=september+30+2025&output=json`
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=october+21+2025&output=json`
   );
   const gBannerTime = await wolfTime1.json();
   const wolfTime2 = await fetch(
@@ -37,7 +37,7 @@ async function daily() {
   ***Good Day Everyone! Here is Your Daily Hoyo Banner Update:***
     
     :salt: **Banners**
-    ***Genshin Banner | 6.0 Luna 1: Lauma DEBUT, Nahida, Barbara, Kuki, Kaveh***
+    ***Genshin Banner | 6.0 Luna 1 Phase 2: Flins DEBUT, Yelan, Sucrose, Dori, Aino DEBUT***
     Ends on ${gBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
     Time left: ${gBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
     
@@ -66,4 +66,5 @@ async function daily() {
     `;
 }
 module.exports.daily = daily;
+
 
